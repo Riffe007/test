121a2dbf24ee76a7ac63465c582b3e1c4ace6b7b
Timothy Riffe <69171502+Riffe007@users.noreply.github.com>
2026-05-11 14:56:01 -0700

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,5 +1,12 @@
-cd ~/Documents/projects/MetaExecuTorch/executorch-toolkit
-source .venv/bin/activate
-python export/vision/pytorch_to_executorch_vision.py \
-    tests/integration/configs/mobile_net_v2_ssd/config_mobile_net_v2_ssd_basemodel.json \
-    --generate-report
+cd ~/Documents/projects/MetaExecuTorch && python -c "
+import sys, torch
+sys.path.insert(0, 'model_sources/MobileNetV2/src/pytorch/pytorch-ssd')
+from vision.ssd.mobilenet_v2_ssd_lite import create_mobilenetv2_ssd_lite
+m = create_mobilenetv2_ssd_lite(num_classes=21, is_test=True)
+m.load_state_dict(torch.load(
+    'model_sources/MobileNetV2/weights/mb2-ssd-lite-mp-0_686.pth',
+    map_location='cpu', weights_only=False), strict=True)
+m.eval()
+torch.save(m, 'model_sources/MobileNetV2/weights/mobile_net_v2_ssd.pth')
+print('done')
+"
