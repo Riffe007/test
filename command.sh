2ceb653cc7a828da413b338ac0b0c30e724446bd
Timothy Riffe <69171502+Riffe007@users.noreply.github.com>
2026-05-12 16:52:03 -0700

    Update command.sh

--- command.sh
+++ command.sh
@@ -21,3 +21,4 @@ for a in d['annotations'][:3]:
     print(f'  image_id={a[\"image_id\"]} cat={a[\"category_id\"]} bbox={a[\"bbox\"]}')
 print('Sample image:', {k: d['images'][0][k] for k in ('id','file_name','width','height')})
 "
+cd ~/Documents/projects/MetaExecuTorch/executorch-toolkit && source .venv/bin/activate && PYTHONPATH=../model_sources/MobileNetV2/src/pytorch/pytorch-ssd python evaluation/mobilenetv2/evaluate.py --voc-root ~/.cache/kagglehub/datasets/watanabe2362/voctrainval-11may2012/versions/1/VOCdevkit/VOC2012 --gt-coco-json ../dataset/voc2012_as_coco/instances_voc2012_val.json --tflite-model ../model_sources/MobileNetV2/weights/model.tflite --pytorch-model ../model_sources/MobileNetV2/weights/mobile_net_v2_ssd.pth --model-dir tests/integration/outputs/mobile_net_v2_ssd/basemodel_workflow/models --results-dir ../output/eval_mobilenet_v2_ssd --generate-report --max-samples 25 2>&1 | tee ../output/eval_smoke2.log
