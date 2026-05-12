db3714626fb5df6d07e138ec1f08277df5747b61
Timothy Riffe <69171502+Riffe007@users.noreply.github.com>
2026-05-12 11:53:45 -0700

    Update command.sh

--- command.sh
+++ command.sh
@@ -16,3 +16,6 @@ mkdir -p output/baseline_voc_seg && source executorch-toolkit/.venv/bin/activate
 ls ~/.cache/kagglehub/dataset/watanabe2362/voctrainval-11may2012/version/1/VOCdevkit/VOC2012/
 
 python dataset/scripts/voc_to_coco.py --voc-root ~/.cache/kagglehub/datasets/watanabe2362/voctrainval-11may2012/versions/1/VOCdevkit/VOC2012 --split val --output dataset/voc2012_as_coco/instances_voc2012_val.json
+
+
+python executorch-toolkit/evaluation/mobilenetv2/evaluate.py --data-path ~/.cache/kagglehub/datasets/watanabe2362/voctrainval-11may2012/versions/1/VOCdevkit/VOC2012/JPEGImages --coco-annotations dataset/voc2012_as_coco/instances_voc2012_val.json --tflite-model model_sources/MobileNetV2/weights/model.tflite --pytorch-weights model_sources/MobileNetV2/weights/mb2-ssd-lite-mp-0_686.pth --pytorch-source model_sources/MobileNetV2/src/pytorch/pytorch-ssd --results-dir output/baseline_voc --max-samples 5823 2>&1 | tee output/baseline_voc/run.log
