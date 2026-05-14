a3c4a16ecf59e3ca92ef836a6b2a532294c01b4b
Timothy Riffe <69171502+Riffe007@users.noreply.github.com>
2026-05-13 20:58:57 -0700

    Update command.sh

--- command.sh
+++ command.sh
@@ -3,11 +3,11 @@ python -c "
 import json
 p = 'export/configs/vision/config_mobile_net_v2_ssd.json'
 c = json.load(open(p))
-c['export']['benchmark'] = True
-c['export']['benchmark_iterations'] = 50
+c['export']['use_recipes'] = True
 json.dump(c, open(p, 'w'), indent=2)
-print('benchmark=true, iters=50')
+print('use_recipes=true')
 " && \
 source .venv/bin/activate && \
+export PYTHONPATH=\"$(realpath ../model_sources/MobileNetV2/src/pytorch/pytorch-ssd):$PYTHONPATH\" && \
 python export/vision/pytorch_to_executorch_vision.py \
   export/configs/vision/config_mobile_net_v2_ssd.json --generate-report
