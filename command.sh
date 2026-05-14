9210b86416b0de9e72b8faa38d8cc378eea2b0b6
Timothy Riffe <69171502+Riffe007@users.noreply.github.com>
2026-05-13 20:47:32 -0700

    Update command.sh

--- command.sh
+++ command.sh
@@ -3,16 +3,11 @@ python -c "
 import json
 p = 'export/configs/vision/config_mobile_net_v2_ssd.json'
 c = json.load(open(p))
-c['export']['enable_profiling'] = True
+c['export']['benchmark'] = True
+c['export']['benchmark_iterations'] = 50
 json.dump(c, open(p, 'w'), indent=2)
-print('enable_profiling = true')
+print('benchmark=true, iters=50')
 " && \
 source .venv/bin/activate && \
 python export/vision/pytorch_to_executorch_vision.py \
   export/configs/vision/config_mobile_net_v2_ssd.json --generate-report
-
-
-  cd ~/Documents/projects/MetaExecuTorch/executorch-toolkit && \
-source .venv/bin/activate && \
-python evaluation/mobilenetv2/evaluate.py \
-  --config export/configs/vision/config_mobile_net_v2_ssd.json
