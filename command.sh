9a059dad6da0431e3609e0dc0cdb49fcc7f8e046
Timothy Riffe <69171502+Riffe007@users.noreply.github.com>
2026-05-15 11:28:55 -0700

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,5 +1,3 @@
-python -m export.vision.pytorch_to_executorch_vision \
-  export/configs/vision/config_mobile_net_v2_ssd.json --generate-report
 mkdir -p ~/Documents/projects/MetaExecuTorch/model_sources/MobileNetV2_TFLite
 cp "/mnt/c/Users/timothy_riffe/Downloads/tf2_ssd_mobilenet_v2_coco17_ptq.tflite" \
    ~/Documents/projects/MetaExecuTorch/model_sources/MobileNetV2_TFLite/
