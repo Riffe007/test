8c82eebee33faa4dee5a90fa88bc0c421d396e9d
Timothy Riffe <69171502+Riffe007@users.noreply.github.com>
2026-05-15 13:07:30 -0700

    Update command.sh

--- command.sh
+++ command.sh
@@ -2,3 +2,11 @@ mkdir -p ~/Documents/projects/MetaExecuTorch/model_sources/MobileNetV2_TFLite
 cp "/mnt/c/Users/timothy_riffe/Downloads/tf2_ssd_mobilenet_v2_coco17_ptq.tflite" \
    ~/Documents/projects/MetaExecuTorch/model_sources/MobileNetV2_TFLite/
 ls ~/Documents/projects/MetaExecuTorch/model_sources/MobileNetV2_TFLite/
+
+
+
+find ~/Documents/projects/MetaExecuTorch -type d -name "vision" 2>/dev/null
+
+
+python evaluation/mobilenetv2/compare_tflite_pytorch_coco.py \
+    --qfgaohao-repo /correct/path/here
